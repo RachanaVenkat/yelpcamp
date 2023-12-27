2fe354082da03dbb14cb147fa036a77e017089fc
“Rachana” <rachanaa.v.01@gmail.com>
2023-12-27 22:09:20 +0530

    yelpcamp

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const Campground=require('./models/campground')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')//engine used to parse ejs
+const catchAsync = require('./utils/catchAsync')
 
 mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp') //here, yelp-camp is the name of the db to access in mongosh
 
@@ -27,10 +28,10 @@ app.get('/',(req,res)=>{
     res.render('home')//renders the home.ejs
 })
 
-app.get('/campgrounds',async (req,res)=>{
+app.get('/campgrounds',catchAsync(async (req,res)=>{
     const campgrounds = await Campground.find({})
     res.render('campgrounds/index',{campgrounds})//campgrounds folder in views
-})
+}))
 
 //ORDER MATTERS HERE
 app.get('/campgrounds/new',(req,res)=>{
@@ -38,34 +39,39 @@ app.get('/campgrounds/new',(req,res)=>{
 })
 //WHEN ITS AFTER THE FINDBYID,IT TREATS "NEW" AS AN ID AND THROWS ERROR SINCE IT DOESN'T EXIST
 
-app.post('/campgrounds',async(req,res)=>{
+app.post('/campgrounds',catchAsync(async(req,res,next)=>{
     const campground = new Campground(req.body.campground)//cuz the parser(urlenconded shit) returns an object in which campground is the key fr the req values
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
-})
+    
+}))
 
-app.get('/campgrounds/:id',async (req,res)=>{
+app.get('/campgrounds/:id',catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/show',{campground})
 
-})
+}))
 
-app.get('/campgrounds/:id/edit',async (req,res)=>{
+app.get('/campgrounds/:id/edit',catchAsync(async (req,res)=>{
     const campground = await Campground.findById(req.params.id)
     res.render('campgrounds/edit',{campground})
-})
+}))
 
-app.put('/campgrounds/:id',async(req,res)=>{
+app.put('/campgrounds/:id',catchAsync(async(req,res)=>{
     const {id} = req.params                          //req.body.campground cuz all are grouped under campground in ejs
     const campground = await Campground.findByIdAndUpdate(id,{...req.body.campground})//spread expands the iterable
     res.redirect(`/campgrounds/${campground._id}`)
 
-})
+}))
 
-app.delete('/campgrounds/:id', async (req,res)=>{
+app.delete('/campgrounds/:id', catchAsync(async (req,res)=>{
     const {id} = req.params
     await Campground.findByIdAndDelete(id)
     res.redirect('/campgrounds')
+}))
+
+app.use((err,req,res,next)=>{
+    res.send("smtg went wrong")
 })
 
 app.listen(3000, ()=>{
