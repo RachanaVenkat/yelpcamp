ea6c8fc68c21cd964f309eb5cd544a40f6c80f2d
“Rachana” <rachanaa.v.01@gmail.com>
2023-12-28 18:32:06 +0530

    joi validation

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const Joi = require('joi')
 const Campground=require('./models/campground')
 const methodOverride = require('method-override')
 const ejsMate = require('ejs-mate')//engine used to parse ejs
@@ -41,7 +42,21 @@ app.get('/campgrounds/new',(req,res)=>{
 //WHEN ITS AFTER THE FINDBYID,IT TREATS "NEW" AS AN ID AND THROWS ERROR SINCE IT DOESN'T EXIST
 
 app.post('/campgrounds',catchAsync(async(req,res,next)=>{
-    if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
+    //if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
+    const campgroundSchema = Joi.object({
+        campground: Joi.object({
+            title: Joi.string().required(),
+            price: Joi.number().required().min(0),
+            image: Joi.string.required(),
+            location: Joi.string.required(),
+            description: Joi.string.required()
+        }).required()
+    })
+    const {error} = campgroundSchema.validate(req.body)
+    if(error){
+        const msg = error.details.map(el => el.message).join(',')
+        throw new ExpressError(msg,400)
+    }
     const campground = new Campground(req.body.campground)//cuz the parser(urlenconded shit) returns an object in which campground is the key fr the req values
     await campground.save()
     res.redirect(`/campgrounds/${campground._id}`)
