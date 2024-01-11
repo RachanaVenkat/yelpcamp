af3d2611945ed53f5dcf7409124e9c79df2835fb
“Rachana” <rachanaa.v.01@gmail.com>
2024-01-11 14:17:00 +0530

    geocoding added

--- controllers/campgrounds.js
+++ controllers/campgrounds.js
@@ -1,4 +1,8 @@
 const Campground=require('../models/campground')
+const mbxGeocoding = require("@mapbox/mapbox-sdk/services/geocoding")
+const mapBoxToken = process.env.mapbox_token
+const geocoder = mbxGeocoding({accessToken: mapBoxToken})
+const {cloudinary} = require('../cloudinary')
 
 module.exports.index = async (req,res)=>{
     const campgrounds = await Campground.find({})
@@ -11,7 +15,12 @@ module.exports.renderNewForm = (req,res)=>{
 
 module.exports.createCampground = async(req,res,next)=>{
     //if(!req.body.campground) throw new ExpressError('Invalid Campground Data',400)
+    const geoData = await geocoder.forwardGeocode({
+        query: req.body.campground.location,
+        limit: 1
+    }).send()
     const campground = new Campground(req.body.campground)//cuz the parser(urlenconded shit) returns an object in which campground is the key fr the req values
+    campground.geometry = geoData.body.features[0].geometry
     campground.images = req.files.map(f=>({url: f.path, filename: f.filename}))
     campground.author = req.user._id//req.user from passport
     await campground.save()
@@ -47,6 +56,7 @@ module.exports.renderEditForm = async (req,res)=>{
 
 module.exports.updateCampground = async(req,res)=>{
     const {id} = req.params   
+    console.log(req.body)
     const campground = await Campground.findById(id)                       //req.body.campground cuz all are grouped under campground in ejs
     if(!campground){
         req.flash('error','Cannot find that campground')
@@ -56,6 +66,13 @@ module.exports.updateCampground = async(req,res)=>{
     const imgs = req.files.map(f=>({url: f.path, filename: f.filename}))
     campground.images.push(...imgs)
     await campground.save()
+    if(req.body.deleteImages){
+        for(let filename of req.body.deleteImages){
+            await cloudinary.uploader.destroy(filename)
+        }
+        await campground.updateOne({ $pull: { images: { filename: {$in: req.body.deleteImages}}}})
+        console.log(campground)
+    }
     req.flash('success','Successfully updated the campground')
     res.redirect(`/campgrounds/${campground._id}`)
 }
--- models/campground.js
+++ models/campground.js
@@ -2,14 +2,29 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 const Review = require('./reviews')
 
+const imageSchema = new Schema({
+    url: String,
+    filename: String
+})
+//virtual is used to avoid making a copy of the already existing url, but is present virtually in db
+imageSchema.virtual('thumbnail').get(function(){
+    return this.url.replace('/upload','/upload/w_200')//reduces width to 200
+})
+
 const CampgroundSchema = new Schema({
     title:String,
-    images:[
-        {
-        url: String,
-        filename: String
+    images:[imageSchema ],
+    geometry: {
+        type: {
+            type: String,
+            enum: ['Point'],
+            required: true
+        },
+        coordinates: {
+            type: [Number],
+            required: true
         }
-    ],
+    },
     price:Number,
     description:String,
     location:String,
